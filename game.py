08b0a5052921c5c432aac6869d45c165e1ee5d2e
Benjamin Greenawald <bgreenawald@gmail.com>
2020-04-04 20:34:38 -0400

    Added locking mechanism to promote synchronization

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import datetime
 import random
 from enum import Enum
+from threading import Lock
 from typing import Any, Dict
 
 
@@ -53,7 +54,8 @@ class Game(object):
         tiles_remaining (int): The number of tiles left in the game.
         num_players (int): The number of players.
         player_tiles (Dict[str, List[str]]): A dictionary mapping each player to their tiles.
-        last_peel (datetime): The time of the last peel, used to prevent overlapping peels.
+        last_peel (datetime.datetime): The time of the last peel, used to prevent overlapping peels.
+        lock (threading.Lock): A lock to keep the game state synchronized.
 
     """
 
@@ -80,6 +82,9 @@ class Game(object):
         # Player tile tracking
         self.players = {}
 
+        # Lock for synchronization
+        self.lock = Lock()
+
     def reset(self,):
         # Tile generation
         self.tiles = []
@@ -100,6 +105,9 @@ class Game(object):
         # Reset player tiles
         for player in self.players:
             self.players[player] = []
+
+        # Lock for synchronization
+        self.lock = Lock()
 
     def __str__(self) -> str:  # pragma: no cover
         return (
@@ -139,14 +147,18 @@ class Game(object):
     def start_game(self):
         """Starts the game by setting the number of players and divvying out tiles.
         """
-        if self.state != State.IDLE:
-            raise GameException(
-                f"Cannot start game, game state is {self.state} when it shoudl be 'IDLE'"
-            )
-        else:
-            self.num_players = len(self.players)
-            self._divy_out_tiles()
-            self.state = State.HIDDEN
+        self.lock.acquire()
+        try:
+            if self.state != State.IDLE:
+                raise GameException(
+                    f"Cannot start game, game state is {self.state} when it shoudl be 'IDLE'"
+                )
+            else:
+                self.num_players = len(self.players)
+                self._divy_out_tiles()
+                self.state = State.HIDDEN
+        finally:
+            self.lock.release()
 
     def _divy_out_tiles(self):
         """Divy out the correct number of tiles to each player.
@@ -174,39 +186,49 @@ class Game(object):
     def split(self):
         """Start the game action.
         """
-        if self.state != State.HIDDEN:
-            raise GameException(
-                f"Cannot split, game state is {self.state} when it should be 'HIDDEN'"
-            )
-        else:
-            self.state = State.ACTIVE
+        self.lock.acquire()
+        try:
+            if self.state != State.HIDDEN:
+                raise GameException(
+                    f"Cannot split, game state is {self.state} when it should be 'HIDDEN'"
+                )
+            else:
+                self.state = State.ACTIVE
+        finally:
+            self.lock.release()
 
     def peel(self):
         """Give a new tile to each player.
         """
-        if self.state != State.ACTIVE:
-            raise GameException(
-                f"Cannot peel, game state is {self.state} when it should be 'ACTIVE'"
-            )
-        else:
-            # Make sure a peel hasn't happened within a second to prevent overlap
-            if (datetime.datetime.now() - self.last_peel).seconds <= 1:
-                raise GameException("Peel occuring too frequently.")
-            # Make sure there are enough tiles to make a peel.
-            if self.num_players > self.tiles_remaining:
-                raise GameException("Not enough tiles to deal.")
+        self.lock.acquire()
+        try:
+            if self.state != State.ACTIVE:
+                raise GameException(
+                    f"Cannot peel, game state is {self.state} when it should be 'ACTIVE'"
+                )
+            else:
+                # Make sure a peel hasn't happened within a fraction of a second to prevent overlap
+                if (datetime.datetime.now() - self.last_peel).seconds <= 1:
+                    raise GameException("Peel occuring too frequently.")
 
-            # Give a new tile to each player.
-            for player in self.players:
-                self.players[player].append(self.tiles.pop())
-            self.tiles_remaining = len(self.tiles)
+                # Update the time of the last peel
+                self.last_peel = datetime.datetime.now()
 
-            # See if we there are enough tiles left for another peel
-            if self.tiles_remaining < self.num_players:
-                self.state = State.ENDGAME
+                # Make sure there are enough tiles to make a peel.
+                if self.num_players > self.tiles_remaining:
+                    raise GameException("Not enough tiles to deal.")
 
-            # Update the time of the last peel
-            self.last_peel = datetime.datetime.now()
+                # Give a new tile to each player.
+                for player in self.players:
+                    self.players[player].append(self.tiles.pop())
+                self.tiles_remaining = len(self.tiles)
+
+                # See if we there are enough tiles left for another peel
+                if self.tiles_remaining < self.num_players:
+                    self.state = State.ENDGAME
+
+        finally:
+            self.lock.release()
 
     def swap(self, letter, player):
         """Swap a given tile for three tiles for a player.
@@ -218,48 +240,60 @@ class Game(object):
         Raises:
             GameException: The player does not have an instance of that tile.
         """
-        if self.state not in [State.ACTIVE, State.ENDGAME]:
-            raise GameException(
-                f"Cannot swap, game state is {self.state} when it should be 'ACTIVE' or 'ENDGAME'"
-            )
-        else:
-            if letter not in self.players[player]:
-                raise GameException("Player does not have this letter to remove.")
+        self.lock.acquire()
+        try:
+            if self.state not in [State.ACTIVE, State.ENDGAME]:
+                raise GameException(
+                    f"Cannot swap, game state is {self.state} when it should be 'ACTIVE' or 'ENDGAME'"
+                )
+            else:
+                if letter not in self.players[player]:
+                    raise GameException("Player does not have this letter to remove.")
 
-            # Add three new tiles to the player, or all remaining tiles if less than 3 remain.
-            for _ in range(min(3, self.tiles_remaining)):
-                self.players[player].append(self.tiles.pop())
+                # Add three new tiles to the player, or all remaining tiles if less than 3 remain.
+                for _ in range(min(3, self.tiles_remaining)):
+                    self.players[player].append(self.tiles.pop())
 
-            # Remove one instance of the given letter from the player
-            self.players[player].remove(letter)
+                # Remove one instance of the given letter from the player
+                self.players[player].remove(letter)
 
-            # Add the letter back to the tile pile and shuffle
-            self.tiles.append(letter)
-            random.shuffle(self.tiles)
+                # Add the letter back to the tile pile and shuffle
+                self.tiles.append(letter)
+                random.shuffle(self.tiles)
 
-            # Update number of tiles
-            self.tiles_remaining = len(self.tiles)
+                # Update number of tiles
+                self.tiles_remaining = len(self.tiles)
 
-            # Check if peels can still occur
-            if self.tiles_remaining < self.num_players:
-                self.state = State.ENDGAME
+                # Check if peels can still occur
+                if self.tiles_remaining < self.num_players:
+                    self.state = State.ENDGAME
+        finally:
+            self.lock.release()
 
     def bananagrams(self):
         """End the game (someone has bananagrams)
         """
-        if self.state != State.ENDGAME:
-            raise GameException(
-                f"Cannot call bananagrams, game state is {self.state} when it should be 'ENDGAME'"
-            )
-        else:
-            self.state = State.OVER
+        self.lock.acquire()
+        try:
+            if self.state != State.ENDGAME:
+                raise GameException(
+                    f"Cannot call bananagrams, game state is {self.state} when it should be 'ENDGAME'"
+                )
+            else:
+                self.state = State.OVER
+        finally:
+            self.lock.release()
 
     def continue_game(self):
         """Continue the game (false alarm on banagrams)
         """
-        if self.state != State.OVER:
-            raise GameException(
-                f"Cannot continue game, game state is {self.state} when it should be 'OVER'"
-            )
-        else:
-            self.state = State.ENDGAME
+        self.lock.acquire()
+        try:
+            if self.state != State.OVER:
+                raise GameException(
+                    f"Cannot continue game, game state is {self.state} when it should be 'OVER'"
+                )
+            else:
+                self.state = State.ENDGAME
+        finally:
+            self.lock.release()

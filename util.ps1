36e595074379615aa0befcec48ca4206e3c75a1a
Jocelyn Huang <seaweed4@live.com>
2020-04-10 19:51:37 -0400

    Rename stylesheets

--- util.ps1
+++ util.ps1
@@ -1,5 +1,4 @@
 function Build-Static {
     sass "static\styles\scss\styles.scss" "static\styles\css\styles.css";
     sass "static\styles\scss\game.scss" "static\styles\css\game.css";
-    sass "static\styles\scss\board.scss" "static\styles\css\board.css";
 }

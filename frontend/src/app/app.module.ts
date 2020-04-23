d0dafb9178bdeb47d270798f5867a841786c0730
Jocelyn Huang <seaweed4@live.com>
2020-04-23 00:33:26 -0400

    Initialize components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
+import { BoardComponent } from './components/board/board.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LobbyComponent,
+    BoardComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,

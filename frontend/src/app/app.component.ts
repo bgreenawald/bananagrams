345eadb08ea5bf573435b50307e5b2670bee1b37
Jocelyn Huang <seaweed4@live.com>
2020-04-26 23:10:08 -0400

    update gameID  upon routing changes -- in frontend/app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Socket }
+  from 'ngx-socket-io';
+import { SocketService } from './services/socket.service';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  public gameID: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private socket: Socket,
+    private socketService: SocketService
+  ) { }
+  ngOnInit() {
+    // this.socketService.socketInit();
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.setGameID(event.url);
+      }
+    }
+    )
+  }
+
+  // setGameID = () => {
+  //   console.log('snapshot', this.route.snapshot)
+  //   const id = +this.route.snapshot.paramMap.get('id');
+  //   console.log('snapshot id', id)
+  //   this.gameID = id.toString();
+  // }
+
+  setGameID = (url: string) => {
+    if (url.includes('game') || url.includes('lobby')) {
+      let gameID = url.split('/')[2];
+      this.gameID = gameID;
+    }
+  }
 }

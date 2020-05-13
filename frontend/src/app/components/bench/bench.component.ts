09d4c09dd15d643c80d50a284419e5001d9151ab
Jocelyn Huang <seaweed4@live.com>
2020-05-12 20:45:20 -0400

    Remove defunct code

--- frontend/src/app/components/bench/bench.component.ts
+++ frontend/src/app/components/bench/bench.component.ts
@@ -9,19 +9,11 @@ import { AppComponent } from '../../app.component';
 })
 export class BenchComponent implements OnInit {
   @Input() tiles: string[];
-  // @Input() index: number[];
 
   constructor(
     private app: AppComponent
   ) { }
 
   ngOnInit(): void {
-    console.log(this.tiles)
-    // this._generateTileIndex();
   }
-
-  // private _generateTileIndex = () => {
-  //   const length = this.tiles.length;
-  //   this.index = Array(length).fill(null).map((x, i) => i);
-  // }
 }

ec89cf5775224fc4112df1ffeb9083fc58e459d0
Benjamin Greenawald <bgreenawald@gmail.com>
2020-06-08 10:21:26 -0400

    Transition to Elastic Beanstalk

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn --bind 0.0.0.0:${PORT} --worker-class eventlet -w 1 'app:app'
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: "http://localhost:8080"
+  backendUrl: "http://chalkful.us-east-1.elasticbeanstalk.com"
 };
